00f51696b80ff5b5c18d71bf0196698b5685cd18
MrJoel2021 <55717039+MrJoel2021@users.noreply.github.com>
2026-07-20 12:18:48 +0100

    Add fillable fields to Job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,7 @@ class Job extends Model
 {
     // Tell Laravel this model uses the job_listings table
     protected $table = 'job_listings';
+
+    // Allow these fields to be inserted using Job::create()
+    protected $fillable = ['title', 'salary'];
 }
